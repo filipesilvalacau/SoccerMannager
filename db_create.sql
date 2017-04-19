a27fd22ddd4d13e7bb86989147b9c77394368c4b
Bruna Nobre Almeida <brunanobrealmeida@gmail.com>
2017-04-18 21:52:14 -0300

    Novo Banco de dados

--- db_create.sql
+++ db_create.sql
@@ -2,43 +2,59 @@ CREATE DATABASE soccer_manager;
 
 USE soccer_manager;
 
-CREATE TABLE account (
-	id_account INTEGER PRIMARY KEY AUTO_INCREMENT,
-	balance	   DECIMAL(5,2)
+CREATE TABLE `account` (
+  `id_account` int(11) NOT NULL,
+  `balance` decimal(5,2) DEFAULT NULL,
+  `id_client` int(11) DEFAULT NULL,
+  `id_employee` int(11) DEFAULT NULL
 );
 
-CREATE TABLE client (
-	id_client 	INTEGER PRIMARY KEY AUTO_INCREMENT,
-	name		VARCHAR(150),
-	address		VARCHAR(255),
-	cpf			VARCHAR(13),
-	login		VARCHAR(45),
-	password	VARCHAR(10),
-	privilage	CHAR(1),
-	id_account  INTEGER,
-	FOREIGN KEY (id_account) REFERENCES account(id_account)
+
+
+CREATE TABLE `client` (
+  `id_client` int(11) NOT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `cpf` varchar(13) DEFAULT NULL,
+  `login` varchar(45) DEFAULT NULL,
+  `password` varchar(10) DEFAULT NULL
 );
 
-CREATE TABLE phone (
-	id_client INTEGER,
-	number	NUMERIC,
-	FOREIGN KEY (id_client) REFERENCES client(id_client)
+
+
+CREATE TABLE `court` (
+  `id_court` int(11) NOT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `price` decimal(5,2) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL
 );
 
-CREATE TABLE court (
-	id_court	INTEGER PRIMARY KEY AUTO_INCREMENT,
-	type		VARCHAR(45),
-	price		DECIMAL(5,2),
-	name		VARCHAR(45)
+
+CREATE TABLE `employee` (
+  `id_employee` int(11) NOT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `cpf` varchar(13) DEFAULT NULL,
+  `login` varchar(45) DEFAULT NULL,
+  `password` varchar(10) DEFAULT NULL
 );
 
-CREATE TABLE rental (
-	id_rental	INTEGER PRIMARY KEY AUTO_INCREMENT,
-	rental_date DATE,
-	hour	TIME,
-	fee		DECIMAL(5,2),
-	id_client	INTEGER,
-	id_court	INTEGER,
-	FOREIGN KEY (id_client) REFERENCES client(id_client),
-	FOREIGN KEY (id_court) REFERENCES court(id_court)
+
+
+CREATE TABLE `phone` (
+  `id_client` int(11) DEFAULT NULL,
+  `number` decimal(10,0) DEFAULT NULL
 );
+
+
+
+CREATE TABLE `rental` (
+  `id_rental` int(11) NOT NULL,
+  `rental_date` date DEFAULT NULL,
+  `hour` time DEFAULT NULL,
+  `fee` decimal(5,2) DEFAULT NULL,
+  `id_client` int(11) DEFAULT NULL,
+  `id_court` int(11) DEFAULT NULL
+);
+
+
